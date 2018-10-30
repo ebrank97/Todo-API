2e34a2c8c5b1d320a4bee8e6d8d224f1ec6be10b
Ebran Khan <ebrank97@gmail.com>
2018-10-30 10:38:03 +0530

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,48 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {
+  MongoClient,
+  ObjectID
+} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB Server');
+
+  // deleteMany
+  // db.collection('Todos').deleteMany({
+  //   text: 'Its time to go to the college'
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  // deleteOne
+  // db.collection('Todos').deleteOne({
+  //   text: 'Its time to go to the college'
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  // findOneAndDelete
+  // db.collection('Todos').findOneAndDelete({
+  //   completed: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  // Challange //
+  // db.collection('Users').deleteMany({
+  //   name: 'Ebran'
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndDelete({
+    _id: new ObjectID('5bcf0b73934af52920845248')
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // db.close();
+});
