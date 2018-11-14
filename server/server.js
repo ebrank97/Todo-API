fc68c01eee9b46851c04754140762530e5263cd4
Ebran Khan <ebrank97@gmail.com>
2018-11-15 00:31:11 +0530

    Add POST /todos rpute and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,32 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+
+let {
+  mongoose
+} = require('./db/mongoose');
+let {
+  Todo
+} = require('./models/Todo');
+let {
+  User
+} = require('./models/User');
+
+let app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+  let todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Started on Port 3000');
+});
